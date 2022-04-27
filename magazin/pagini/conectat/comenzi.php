18ea8713e66d90fbb2e3f7bed4c2cf93080de0fd
IuliaMariaDascalu <104492394+IuliaMariaDascalu@users.noreply.github.com>
2022-04-27 11:41:09 +0300

    Update comenzi.php

--- magazin/pagini/conectat/comenzi.php
+++ magazin/pagini/conectat/comenzi.php
@@ -1,15 +1,13 @@
 <?php
 
 $user = preiaUtilizatorDupaEmail($_SESSION['user']);
-//var_dump(preiaComenziDupaUserId($user['id']));
 $comenzi = preiaComenziDupaUserId($user['id']);
 
 foreach ($comenzi as $index => $comanda) {
-   // var_dump($comanda);die();
     print "Comanda #" . $comanda['id'] ." <br>";
     print $comanda['data_plasare'];
     print '<br>';
-    $produse = json_decode($comanda['produse'], true);//a redevenit array
+    $produse = json_decode($comanda['produse'], true);
     print 'Produse:<br>';
     print '<ol>';
     foreach ($produse as $produs) {
